b07d7172ce38b1c6324be81b153c7a21baedbfa3
ostsam <ost.sam@gmail.com>
2025-08-09 18:29:38 -0400

    merge sort

--- problem-3.js
+++ problem-3.js
@@ -1,11 +1,18 @@
 const merge = (arr1, arr2) => {
   let arr3 = [];
   let i = 0;
+  let j = 0;
 
-  while (arr1[i] < arr2[i]) {
-    arr3.push(arr1[i]);
-    i++;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      arr3.push(arr1[i]);
+      i++;
+    } else {
+      arr3.push(arr2[j]);
+      j++;
+    }
   }
+  arr3.push(arr2[j])
   return arr3;
 };
 
